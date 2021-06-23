65e3c785bc006b1c775c9dda14e4af497c8f2ca5
Travis Bostick <traviswbostick@gmail.com>
2021-06-23 18:17:19 -0500

    Put env variables in .env

--- server/index.js
+++ server/index.js
@@ -1,7 +1,24 @@
 import express from 'express';
 import admin from 'firebase-admin';
 import cors from 'cors';
-import serviceAccount from './serviceAccountKey.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const serviceAccount = {
+  type: 'service_account',
+  project_id: 'pro-cycling-schedule',
+  private_key_id: process.env.FIRESTORE_PRIVATE_KEY_ID,
+  private_key: process.env.FIRESTORE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIRESTORE_CLIENT_EMAIL,
+  client_id: process.env.FIRESTORE_CLIENT_ID,
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-uilw4%40pro-cycling-schedule.iam.gserviceaccount.com'
+};
+
+// console.log(serviceAccount);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
